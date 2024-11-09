256518aee191947dbe864201cc00113e25f8d6b1
Mahir Shahriyar <mahir.shahriyar.polok@gmail.com>
2024-11-10 02:14:23 +0600

    Done

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'detail'
+    ];
+
+    public static function find(int $id)
+    {
+    }
+}
